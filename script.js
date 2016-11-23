9bf1f627c3b5db8dcd59b1667b209f77087aa11f
WalnutsAreCool <claadam@nuevaschool.org>
2016-11-23 08:58:37 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 console.log("hi")
 var commentInput = $("#comment-input")
 var commentDisplay = $(".commment-display")
+var newComm
 
 commentInput.keydown(function(e) {
   if (e.which == 13) {
     console.log("hi also");
     addListItem("comments", commentInput.val());
+
   }
 })
 
-onNewListItem(commentInput, function(newComment) {
+
+
+onNewListItem("commentInput", function(newComment) {
   commentDisplay.append("<span>" + newComment + "</span>")
 })
