1848a34efc04a06b31985d1e30724a093f9e5c3a
WalnutsAreCool <claadam@nuevaschool.org>
2016-11-23 08:38:47 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var commentInput = $("#comment-input")
-var commentDisplay = $(".not-actually-the-right-class")
+var commentInput = $("#form-control")
+var commentDisplay = $(".comment ")
 
 commentInput.keydown(function(e) {
   if (e.which == 13) {
